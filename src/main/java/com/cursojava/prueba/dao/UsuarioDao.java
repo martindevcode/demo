ff4742831ced02b900952bf49ffc42c37d2b593f
Martin Gonzales <eduardo_martin93@hotmail.com>
2026-05-04 23:26:01 -0500

    se añadió query cursojava.sql y cambio por lombok

---
+++ src/main/java/com/cursojava/prueba/dao/UsuarioDao.java
@@ -0,0 +1,10 @@
+package com.cursojava.prueba.dao;
+
+import com.cursojava.prueba.models.Usuario;
+
+import java.util.List;
+
+public interface UsuarioDao {
+
+    List<Usuario> getUsuarios();
+}
---
+++ src/main/java/com/cursojava/prueba/dao/UsuarioDaoImp.java
@@ -0,0 +1,25 @@
+package com.cursojava.prueba.dao;
+
+import com.cursojava.prueba.models.Usuario;
+import jakarta.persistence.PersistenceContext;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import jakarta.persistence.EntityManager;
+
+import java.util.List;
+
+@Repository
+@Transactional
+public class UsuarioDaoImp implements UsuarioDao {
+
+    @PersistenceContext
+    EntityManager entityManager;
+
+    @Override
+    public List<Usuario> getUsuarios() {
+        String query = "FROM Usuario";
+        return entityManager.createQuery(query).getResultList();
+
+    }
+}
