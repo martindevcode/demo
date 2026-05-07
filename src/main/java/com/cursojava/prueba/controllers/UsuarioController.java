5e98cd3ae0f4c578aeb187502a4961d03859c8d6
Martin Gonzales <eduardo_martin93@hotmail.com>
2026-05-06 20:24:12 -0500

    se realizó injección de depencias como agregó listar y eliminar usuarios

--- src/main/java/com/cursojava/prueba/controllers/UsuarioController.java
+++ src/main/java/com/cursojava/prueba/controllers/UsuarioController.java
@@ -1,8 +1,11 @@
 package com.cursojava.prueba.controllers;
 
+import com.cursojava.prueba.dao.UsuarioDao;
 import com.cursojava.prueba.models.Usuario;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -16,7 +19,10 @@ public class UsuarioController {
         return "prueba";
     }*/
 
-    @RequestMapping(value = "usuario/{id}")
+    @Autowired
+    private UsuarioDao usuarioDao;
+
+    @RequestMapping(value = "api/usuario/{id}", method = RequestMethod.GET)
     public Usuario getUsuario(@PathVariable Long id) {
         Usuario usuario = new Usuario();
         usuario.setId(id);
@@ -27,34 +33,9 @@ public class UsuarioController {
         return usuario;
     }
 
-    @RequestMapping(value = "usuarios")
+    @RequestMapping(value = "api/usuarios")
     public List<Usuario> getUsuarios() {
-        List<Usuario> usuarios = new ArrayList<>();
-        Usuario usuario = new Usuario();
-        usuario.setId(25L);
-        usuario.setNombre("Martin");
-        usuario.setApellido("Gonzales");
-        usuario.setEmail("edumar.gonzales@gmail.com");
-        usuario.setTelefono("943762453");
-
-        Usuario usuario2 = new Usuario();
-        usuario2.setId(30L);
-        usuario2.setNombre("Martina");
-        usuario2.setApellido("Gonza");
-        usuario2.setEmail("gonzales@gmail.com");
-        usuario2.setTelefono("123654789");
-
-        Usuario usuario3 = new Usuario();
-        usuario3.setId(45L);
-        usuario3.setNombre("nuevo");
-        usuario3.setApellido("moreno");
-        usuario3.setEmail("edumar@gmail.com");
-        usuario3.setTelefono("741258963");
-
-        usuarios.add(usuario);
-        usuarios.add(usuario2);
-        usuarios.add(usuario3);
-        return usuarios;
+        return usuarioDao.getUsuarios();
     }
 
 
@@ -68,14 +49,9 @@ public class UsuarioController {
         return usuario;
     }
 
-    @RequestMapping(value = "usuario2")
-    public Usuario eliminar() {
-        Usuario usuario = new Usuario();
-        usuario.setNombre("Martin");
-        usuario.setApellido("Gonzales");
-        usuario.setEmail("edumar.gonzales@gmail.com");
-        usuario.setTelefono("943762453");
-        return usuario;
+    @RequestMapping(value = "api/usuario/{id}", method = RequestMethod.DELETE)
+    public void eliminar(@PathVariable Long id) {
+        usuarioDao.eliminar(id);
     }
 
     @RequestMapping(value = "usuario3")
--- src/main/resources/static/js/demo/usuarios.js
+++ src/main/resources/static/js/demo/usuarios.js
@@ -10,7 +10,7 @@ $(document).ready(function() {
 
 async function cargarUsuarios(){
 
-  const request = await fetch('usuarios', {
+  const request = await fetch('api/usuarios', {
     method: 'GET',
     headers: {
       'Accept': 'application/json',
@@ -21,13 +21,20 @@ async function cargarUsuarios(){
 
   //console.log(usuarios);
 
+
  let listadoHtml = '';
 
   for (let usuario of usuarios) {
-  let usuarioHtml = '<tr><td>'+usuario.id+'</td><td>'+usuario.nombre+' '+usuario.apellido+'</td><td>'+usuario.email+'</td><td>'+usuario.telefono+'</td><td><a href="#" class="btn btn-danger btn-circle btn-sm"><i class="fas fa-trash"></i></a></td></tr>';
+  let botonEliminar = '<a href="#" onclick="eliminarUsuario('+usuario.id+')" class="btn btn-danger btn-circle btn-sm"><i class="fas fa-trash"></i></a>';
+
+  let usuarioHtml = '<tr><td>'+usuario.id+'</td><td>'+usuario.nombre+' '+usuario.apellido+'</td><td>'+usuario.email+'</td><td>'+usuario.telefono+'</td><td>'+ botonEliminar +'</td></tr>';
      listadoHtml += usuarioHtml;
   }
 
   document.querySelector('#usuarios tbody').outerHTML = listadoHtml;
 
+}
+
+function eliminarUsuario(id){
+    alert(id);
 }
